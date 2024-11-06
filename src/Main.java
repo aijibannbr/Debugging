b4e6c7b09a105f468bd409abb5321485913721bb
jiban <aijiban.nbr@gmail.com>
2024-11-06 13:05:27 +0600

    Task-5 Debugging Practice

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         DebugPractice debugPractice = new DebugPractice();
         int result = debugPractice.calculate(5, 3);
-        System.out.println("Result: " + result);
+        System.out.println(" Result: " + result);
     }
 }
